3430045cb8cd4d93a2dcf8fe25f2a070f6d29298
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-08-18 19:59:24 +0300

    chore: Update init service to fetch user info along with cart

--- front/src/app/core/services/init.service.ts
+++ front/src/app/core/services/init.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CartService } from './cart.service';
-import { of } from 'rxjs';
+import { forkJoin, of } from 'rxjs';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InitService {
-  constructor(private cartService: CartService) {}
+  constructor(private cartService: CartService, private accountService: AccountService) {}
 
   init() {
     const cartId = localStorage.getItem('cart_id');
     const cart$ = cartId ? this.cartService.getCart(cartId) : of(null);
 
-    return cart$;
+    return forkJoin({
+      cart: cart$,
+      user: this.accountService.getUserInfo(),
+    });
   }
 }
