c35300d33ed394107ad096c3f208c2fdc9565d9d
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-08-26 14:07:26 +0300

    chore: Update checkout component with address step and address pipe

--- front/src/app/components/checkout/review/review.component.ts
+++ front/src/app/components/checkout/review/review.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { CartService } from '../../../core/services/cart.service';
 import { CurrencyPipe } from '@angular/common';
 import { ConfirmationToken } from '@stripe/stripe-js';
+import { AddressPipe } from '../../../shared/pipes/address.pipe';
 
 @Component({
   selector: 'app-review',
   standalone: true,
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, AddressPipe],
   templateUrl: './review.component.html',
   styleUrl: './review.component.scss',
 })
