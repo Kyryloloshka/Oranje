c74065a7328ee1f61b309368dfbd5252694808c1
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-08-28 15:12:06 +0300

    chore: Update database schema with initial migration for Products table

--- Infrastructure/Migrations/20240808115253_init.Designer.cs
+++ Infrastructure/Migrations/20240808115253_init.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(StoreContext))]
-    [Migration("20240807195821_init")]
+    [Migration("20240808115253_init")]
     partial class init
     {
         /// <inheritdoc />
