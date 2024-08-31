32dabb075fc24fa200a20bdeff4049644d43878b
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-08-31 13:55:21 +0300

    chore: Update success component with order details and loading state

--- front/src/app/components/checkout/success/success.component.ts
+++ front/src/app/components/checkout/success/success.component.ts
@@ -1,12 +1,27 @@
+import { CurrencyPipe, DatePipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RouterLink } from '@angular/router';
+import { AddressPipe } from '../../../shared/pipes/address.pipe';
+import { PaymentCardPipe } from '../../../shared/pipes/payment-card.pipe';
+import { OrderService } from '../../../core/services/order.service';
 
 @Component({
   selector: 'app-success',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    RouterLink,
+    MatProgressSpinnerModule,
+    DatePipe,
+    AddressPipe,
+    CurrencyPipe,
+    PaymentCardPipe,
+    NgIf,
+  ],
   templateUrl: './success.component.html',
-  styleUrl: './success.component.scss'
+  styleUrl: './success.component.scss',
 })
 export class SuccessComponent {
-
 }
---
+++ front/src/app/shared/models/order.interface.ts
@@ -0,0 +1,48 @@
+export interface Order {
+  id: number;
+  orderDate: string;
+  buyerEmail: string;
+  shippingAddress: ShippingAddress;
+  deliveryMethod: string;
+  shippingPrice: number;
+  paymentSummary: PaymentSummary;
+  orderItems: OrderItem[];
+  subtotal: number;
+  discount?: number;
+  status: string;
+  total: number;
+  paymentIntentId: string;
+}
+
+export interface ShippingAddress {
+  name: string;
+  line1: string;
+  line2?: string;
+  city: string;
+  state: string;
+  postalCode: string;
+  country: string;
+}
+
+export interface PaymentSummary {
+  last4: number;
+  brand: string;
+  expMonth: number;
+  expYear: number;
+}
+
+export interface OrderItem {
+  productId: number;
+  productName: string;
+  pictureUrl: string;
+  price: number;
+  quantity: number;
+}
+
+export interface OrderToCreate {
+  cartId: string;
+  deliveryMethodId: number;
+  shippingAddress: ShippingAddress;
+  paymentSummary: PaymentSummary;
+  discount?: number;
+}
